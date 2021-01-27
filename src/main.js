d1e13ca26f67bcd43c38b136368f2bc523ae3654
Nir Levy <76614981+nirle97@users.noreply.github.com>
2021-01-28 00:05:56 +0200

    sort button added
    a sort button was added with the function: "sortTasksData".
    The function removes all the content from the list and add it again, sorted by the priority value of each line in the list.

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ let tasksData = [];
 addTaskButton.addEventListener('click', appendDivTask);
 addTaskButton.addEventListener('click', appendTaskToJson);
 addTaskButton.addEventListener('click', () => counter.textContent = viewSection.childElementCount);
+sortButton.addEventListener('click', sortTasksData);
 
 function appendDivTask() {
     if (textInput.value === '') return;
@@ -50,4 +51,35 @@ function appendTaskToJson() {
     console.log(tasksData);
 }
 
+function sortTasksData() {
+    tasksData = tasksData.sort(function (a, b) {return b.priority - a.priority});
+    childrenSortedList = [];
+    for (let i = 0; i < tasksData.length; i++) {
+
+        const todoContainer = document.createElement('div')
+        todoContainer.classList.add("todo-container");
+
+        const todoPriorityContainer = document.createElement('div')
+        todoPriorityContainer.classList.add("todo-priority");
+        todoPriorityContainer.textContent = tasksData[i].priority;
+        todoContainer.appendChild(todoPriorityContainer);
+
+        const todoDateContainer = document.createElement('div')
+        todoDateContainer.classList.add("todo-created-at");
+        todoDateContainer.textContent = tasksData[i].date;
+        todoContainer.appendChild(todoDateContainer);
+        
+        const todoInputContainer = document.createElement('div')
+        todoInputContainer.classList.add("todo-text");
+        todoInputContainer.textContent = tasksData[i].input;
+        todoContainer.appendChild(todoInputContainer);
+
+        childrenSortedList.push(todoContainer); 
+
+    }
+    viewSection.textContent = '';
+    for (div of childrenSortedList) {
+        viewSection.appendChild(div);
+    }
+}
 
