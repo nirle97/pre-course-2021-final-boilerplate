573265d844c2c3fd7ef7238394b70a9d1465b01d
Nir Levy <76614981+nirle97@users.noreply.github.com>
2021-01-28 19:24:20 +0200

    clear all button+func,  change date format
    added clear button and funcion: clear localstorage, input box, myTodo object, zero the counter and empty the viewSection div (the parent of all tasks)
    
    date: now the reload function does not give an updated date - takes the same date from the local storage.
    "en-GB" time format is appended when the plus button is clicked.
    But, the myToDo JSON list, is getting time in millisecond.

--- src/main.js
+++ src/main.js
@@ -11,22 +11,37 @@ const clearButton = document.getElementById("clear");
 
 let myTodo = [];
 
-addTaskButton.addEventListener('click', appendSkeletonDiv);
-addTaskButton.addEventListener('click', appendDataToDiv);
+addTaskButton.addEventListener('click', createSkeletonToMyTodo);
+addTaskButton.addEventListener('click', appendDataToTasksDiv);
 addTaskButton.addEventListener('click', appendTaskToMyTodo);
 addTaskButton.addEventListener('click', appendToLocalStorage);
 addTaskButton.addEventListener('click',CounterFunction);
 sortButton.addEventListener('click', sortMyTodo);
+clearButton.addEventListener('click', clearAll);
 window.addEventListener('load', onReload);
 
+function clearAll() {
+    myTodo = [];
+    viewSection.textContent = '';
+    localStorage.clear();
+    CounterFunction();
+    textInput.value = ''
+}
+
 function onReload() {
-    const valuesOfLocalStorage = JSON.parse(localStorage.getItem('my-todo'));
-    for (let i = 0; i < valuesOfLocalStorage.length; i++) {
-        appendSkeletonDiv();
-        let valuesOfOBject = Object.values(valuesOfLocalStorage[i]);
-        prioritySelector.value = valuesOfOBject[0];
-        textInput.value = valuesOfOBject[2];
-        appendDataToDiv();
+    if (JSON.parse(localStorage.getItem('my-todo')) < 1) return;
+    const objectsOfMyToDo = JSON.parse(localStorage.getItem('my-todo'));
+    for (let i = 0; i < objectsOfMyToDo.length; i++) {
+        createSkeletonToMyTodo();
+        let valuesOfMyToDoObject = Object.values(objectsOfMyToDo[i]);
+        prioritySelector.value = valuesOfMyToDoObject[0];
+        textInput.value = valuesOfMyToDoObject[2];
+        appendDataToTasksDiv();
+        let datesDivs = document.querySelectorAll(".todo-created-at")
+        let dateinMilliseconds = Number(valuesOfMyToDoObject[1]);
+        let sqlDate = (new Date(dateinMilliseconds)).toLocaleString("en-GB").split(',').join(' ');
+        datesDivs[datesDivs.length - 1].textContent = sqlDate;
+        
     }
     myTodo = JSON.parse(localStorage.getItem('my-todo'))
     CounterFunction();
@@ -36,7 +51,7 @@ function CounterFunction() {
     counter.textContent = viewSection.childElementCount;
 }
 
-function appendSkeletonDiv() {
+function createSkeletonToMyTodo() {
     if (textInput.value === '' && localStorage.length === 0 ||
     textInput.value === '' &&  localStorage.length > 0 && myTodo.length > 0) return; // doesn't work when local storage is empty
 
@@ -58,29 +73,37 @@ function appendSkeletonDiv() {
     viewSection.appendChild(todoContainer);
 }
 
-function appendDataToDiv() {
+function appendDataToTasksDiv() {
     if (textInput.value === '' &&  localStorage.length > 0 && myTodo.length > 0) return;
 
     let prioritiesDivs = document.querySelectorAll(".todo-priority");
     prioritiesDivs[prioritiesDivs.length - 1].textContent = prioritySelector.value;
-
-    let datesDivs = document.querySelectorAll(".todo-created-at")
-    let getTime = new Date().getTime();
-    let SqlDate = (new Date(getTime)).toLocaleString("en-GB").split(',').join(' ');
-    datesDivs[datesDivs.length - 1].textContent = SqlDate;
-
+    
     let textDivs = document.querySelectorAll(".todo-text")
     textDivs[textDivs.length - 1].textContent = textInput.value;
-
+    
     textInput.value = '';
+
+    if ((localStorage.length === 0 && myTodo.length === 0)
+        || (localStorage.length > 0 && myTodo.length > 0)) {
+    let datesDivs = document.querySelectorAll(".todo-created-at")
+    let taskDate = new Date();
+    let dateForUser = taskDate.toLocaleString("en-GB");
+    datesDivs[datesDivs.length - 1].textContent = dateForUser;
+    };
 }
 
 function appendTaskToMyTodo() {
     let lastDiv = viewSection.lastChild;
+    const dateForUser = (lastDiv.querySelector(".todo-created-at").textContent);
+    const [first, second] = dateForUser.split(',').map(item => item.trim());
+    const [day, month, year] = first.split('/');
+    const [hours, minutes, seconds] = second.split(':');
+    const dateForMyToDo = new Date(year, month - 1, day, hours, minutes, seconds);
     let taskInfo = {
         "priority": `${lastDiv.querySelector(".todo-priority").textContent}`,
-        "date": `${lastDiv.querySelector(".todo-created-at").textContent}`,
-        "input": `${lastDiv.querySelector(".todo-text").textContent}`
+        "date": `${dateForMyToDo.getTime()}`,
+        "text": `${lastDiv.querySelector(".todo-text").textContent}`
     };
     myTodo.push(taskInfo);
     
@@ -110,12 +133,13 @@ function sortMyTodo() {
         
         const todoInputContainer = document.createElement('div')
         todoInputContainer.classList.add("todo-text");
-        todoInputContainer.textContent = myTodo[i].input;
+        todoInputContainer.textContent = myTodo[i].text;
         todoContainer.appendChild(todoInputContainer);
 
         childrenSortedList.push(todoContainer); 
 
     }
+    textInput.value = '';
     viewSection.textContent = '';
     for (div of childrenSortedList) {
         viewSection.appendChild(div);
